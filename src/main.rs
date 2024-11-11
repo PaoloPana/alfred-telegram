d3d641271a1e33c3b1e4266eff410133de0f4219
Paolo Panarese <panarese.paolo@gmail.com>
2024-11-11 02:08:27 +0100

    updated alfred-rs version

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 use std::collections::{LinkedList, HashMap};
-use std::sync::Arc;
-use alfred_rs::connection::{Receiver, Sender};
 use alfred_rs::error::Error;
 use alfred_rs::AlfredModule;
 use alfred_rs::log::{debug, error, warn};
 use alfred_rs::message::MessageType;
-use alfred_rs::MODULE_INFO_TOPIC_REQUEST;
 use teloxide::Bot;
 use teloxide::prelude::{Message, Requester};
 use teloxide::types::{ChatId, InputFile};
-use tokio::sync::Mutex;
 use teloxide::net::Download;
 use tokio::fs;
 
@@ -21,7 +17,9 @@ const NEW_INCOMING_MESSAGE_TOPIC: &str = "new_incoming_message";
 async fn main() -> Result<(), Error> {
     env_logger::init();
     debug!("Starting telegram module...");
-    let module = AlfredModule::new(MODULE_NAME).await?;
+    let mut module = AlfredModule::new(MODULE_NAME).await?;
+    debug!("Configuring Alfred receiver...");
+    module.listen(RESPONSE_TOPIC).await.expect("Error on alfred subscription");
 
     let bot_token = module.config.get_module_value("bot_token").expect("BOT token not found");
     let callback_topic = module.config.get_module_value("callback");
@@ -30,28 +28,16 @@ async fn main() -> Result<(), Error> {
     }
     let bot = Bot::new(bot_token);
     let bot2 = bot.clone();
-    let alfred_subscriber = Arc::new(Mutex::new(module.connection.subscriber));
-    let alfred_publisher1 = Arc::new(Mutex::new(module.connection.publisher));
-    let alfred_publisher2 = alfred_publisher1.clone();
+    let connection = module.connection.clone();
 
     // TODO: IMPORTANT! refactor with mpsc::channel
     tokio::spawn(async move {
-        let alfred_subscriber = alfred_subscriber.clone();
-        let alfred_publisher = alfred_publisher1.clone();
+        let connection = connection.clone();
         let bot2 = bot2.clone();
         async move {
-            debug!("Configuring Alfred receiver...");
-            alfred_subscriber.lock().await.listen(RESPONSE_TOPIC).await.expect("Error on alfred subscription");
-            debug!("Configured Alfred receiver!!!");
             loop {
                 debug!("Waiting for new Alfred messages...");
-                // TODO: remove all ".expect" and substitute with other
-                let (topic, message) = alfred_subscriber.lock().await.receive().await.expect("Error on receiving Alfred Message");
-                // TODO: add macro for manage_module_info_request
-                if topic == MODULE_INFO_TOPIC_REQUEST {
-                    alfred_publisher.lock().await.send_module_info(MODULE_NAME, &module.capabilities).await.expect("Error replying to MODULE_INFO_TOPIC_REQUEST");
-                    continue;
-                }
+                let (topic, message) = connection.receive().await.expect("Error on receiving Alfred Message");
                 debug!("New message on topic {}: {:?}", topic, message);
                 match topic.as_str() {
                     RESPONSE_TOPIC => {
@@ -80,15 +66,15 @@ async fn main() -> Result<(), Error> {
 
     debug!("Configuring telegram receiver...");
     teloxide::repl(bot.clone(), move |bot: Bot, msg: Message| {
-        let alfred_publisher = alfred_publisher2.clone();
+        let connection = module.connection.clone();
         let callback_topic = callback_topic.clone();
         async move {
             let alfred_msg_res = telegram_msg_to_alfred_msg(msg, &bot).await;
             match alfred_msg_res {
                 Ok(alfred_msg) => {
-                    alfred_publisher.lock().await.send_event(MODULE_NAME, NEW_INCOMING_MESSAGE_TOPIC, &alfred_msg).await.expect("Error on sending new incoming message event");
+                    connection.send_event(MODULE_NAME, NEW_INCOMING_MESSAGE_TOPIC, &alfred_msg).await.expect("Error on sending new incoming message event");
                     if let Some(callback_topic) = callback_topic {
-                        alfred_publisher.lock().await.send(callback_topic.as_str(), &alfred_msg).await.expect("Error on publish");
+                        connection.send(callback_topic.as_str(), &alfred_msg).await.expect("Error on publish");
                     }
                 },
                 Err(err) => {
